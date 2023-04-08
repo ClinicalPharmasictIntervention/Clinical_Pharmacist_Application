d81a631cf9814ed3e68e6a68156e636cef2eee91
abdullah <abdullahMustafa2000>
2023-04-08 13:44:45 +0200

    login elements created

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:clinical_pharmacist_intervention/business_logic/intervention_cubit/cubit.dart';
-import 'package:clinical_pharmacist_intervention/presentation/clinical_pharmacist_screen.dart';
-import 'package:clinical_pharmacist_intervention/presentation/physician_screen.dart';
 import 'package:clinical_pharmacist_intervention/shared/bloc_observer.dart';
 import 'package:clinical_pharmacist_intervention/shared/cubit/cubit.dart';
+import 'package:clinical_pharmacist_intervention/ui/screens/physician_screen.dart';
+import 'package:clinical_pharmacist_intervention/ui/test_ui.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -25,8 +25,17 @@ class MyApp extends StatelessWidget {
         BlocProvider(create: (context) => InterventionCubit()),
       ],
       child: const MaterialApp(
-        home: PhysicianScreen(),
+        home: FakeLanding(),
       ),
     );
   }
+}
+
+
+ThemeData appTheme(BuildContext context) {
+  return Theme.of(context);
+}
+
+TextTheme txtTheme(BuildContext context) {
+  return Theme.of(context).textTheme;
 }
